44d244a80e583e8ea4a5969e2452940a848a8b15
Caiubi Aguiar Coutinho Lima <105654929+CaiubiPK@users.noreply.github.com>
2023-10-26 09:36:53 -0300

    Fase inicial
    Adicionando pasta e primeiro arquivo

---
+++ Rede neutral/Inicio.py
@@ -0,0 +1,32 @@
+
+N = 0.5
+B = 0
+Epoca = 1
+
+while(true)
+    Tamanho = input("Quantos parâmetros?")
+
+
+    for(i=0;i<tamanho;i++)
+    {
+        //preencher os pesos de acordo com o numero de saidas
+    }
+    Tabela = input("Tabela de saída desejada, escreva 'P' para parar")
+
+
+
+    V += saida[n].Tabela[n]
+
+    if(B !- 0)
+    V = V.B
+    // conta com todos os parâmetros
+    
+    
+    
+    //Função de ativação
+    
+    
+    
+    
+    
+    //atualização de pesos
